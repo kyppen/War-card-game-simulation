e9f4d8bbf353c513e3d604ae1c1bd872fccdb539
kyppen <Fagerhus.sondre@gmail.com>
2022-06-03 16:14:09 +0200

    Update wargame.py
    Added feature where the user can decide how many cards is added when a war is called
    Added feature that times how long the game lasts

--- wargame.py
+++ wargame.py
@@ -1,7 +1,8 @@
 #Created by Kyppen
 #Date started 2/6/2022
 #Python version 3.9.7
-import random 
+import random
+import time
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -56,11 +57,18 @@ class Player:
     
     def __str__(self):
         return f"{self.name} has {len(self.all_cards)} cards."
+
+def time_tracking():
+    end_time = time.time()
+    total_time = end_time - starting_time
+    return total_time
+    
     
     
 
 
 if __name__ == "__main__":
+
     checking_for_decks = True
     while checking_for_decks:
         decks = (input("How many decks do you want to play with? [Standard would be 1 that equals 52 cards] "))
@@ -71,7 +79,15 @@ if __name__ == "__main__":
         else:
             print(f"It seems that what you entered isnt a digit, valid input is any number above 0. Given recieved was '{decks}'")
 
+    checking_for_stakes = True
+    while checking_for_stakes:
+        stakes = input("How many cards should be at stake when a war happends? [Standard would be 3 or 5]")
+        if stakes.isdigit() == True:
+            stakes_is_digit = int(stakes)
+            if stakes_is_digit > 0:
+                checking_for_stakes = False
 
+    starting_time = time.time()
     player_one = Player("One")
     player_two = Player("Two")
 
@@ -89,19 +105,24 @@ if __name__ == "__main__":
 
     game_on = True
     round_num = 0
+
     while game_on:
         round_num += 1
         print(f"Round {round_num} Player {player_one} Player {player_two}")
+
+        #Checking if we have a winner
         if len(player_one.all_cards) == 0:
             print("this means this player one lost")
             print("Player Two has won!")
             game_on = False 
+            print(f"This simulation took {time_tracking()} seconds")
             break
         
         if len(player_two.all_cards) == 0:
             print(" this means player two lost")
             print("Player One has won!")
             game_on = False
+            print(f"This simulation took {time_tracking()} seconds")
             break
         
         player_one_cards = []
@@ -111,6 +132,7 @@ if __name__ == "__main__":
         player_two_cards.append(player_two.remove_one())
 
         at_war = True
+
         while at_war:
             if player_one_cards[-1].value > player_two_cards[-1].value:
                 player_one.add_cards(player_one_cards)
@@ -125,19 +147,21 @@ if __name__ == "__main__":
                 at_war = False
             else:
 
-                if len(player_one.all_cards) < 3:
+                if len(player_one.all_cards) < stakes_is_digit:
                     print("player One is unable to play war! Game over at War")
                     print("Player Two Wins!")
+                    print(f"This simulation took {time_tracking()} seconds")
                     game_on = False
                     break
-                elif len(player_two.all_cards) < 3:
+                elif len(player_two.all_cards) < stakes_is_digit:
                     print("Player Two is unable to play War! Game over at war")
                     print("Player One wins!")
+                    print(f"This simulation took {time_tracking()} seconds")
                     game_on = False
                     break
 
                 
-                for x in range(3):
+                for x in range(stakes_is_digit):
                     player_one_cards.append(player_one.remove_one())
                     player_two_cards.append(player_two.remove_one())
 
